e80007bca649d1c1ff64838b144787c4320cf5b8
Lucie Tvrznikova <lt364@Andrew.local>
2014-06-20 11:22:27 -0400

    I am not putting my message in emacs, this is my first commint, ant I've had it add number 1-10 to data1.txt. It works.

---
+++ generate_data.sh
@@ -0,0 +1,8 @@
+# Use a for loop to create a text file called data1.txt with lines 1 through 10
+# Hint: You can append to the end of a file with >>
+# Hint: You can take our simple_forloop.sh, cat that into generate_data.sh, and add to one of the lines to complete this exercise
+
+for i in {1..10};
+do
+    echo $i >> data1.txt
+done
