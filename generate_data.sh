61c3ab2253b4a061d2d48855ca2de4c74efc798d
Lucie Tvrznikova <lt364@Andrew.local>
2014-06-20 11:26:49 -0400

    Added a new comment

--- generate_data.sh
+++ generate_data.sh
@@ -2,6 +2,8 @@
 # Hint: You can append to the end of a file with >>
 # Hint: You can take our simple_forloop.sh, cat that into generate_data.sh, and add to one of the lines to complete this exercise
 
+# Just a new comment
+
 for i in {1..10};
 do
     echo $i >> data1.txt
